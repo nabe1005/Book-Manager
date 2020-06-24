ef207ce0879cc41d55c1b4b77f709f78e01c967b
Taiki Watanabe <taiki.w.1113@gmail.com>
2020-06-24 22:49:24 +0900

    config.pyの編集(dbをheroku運営用に)

--- application/config.py
+++ application/config.py
@@ -6,16 +6,18 @@ load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+
 class DevelopmentConfig:
     # Flask
     DEBUG = True
     SECRET_KEY = os.urandom(24)
 
     # sqlite3
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     # Postgresに切り替え(Heroku運用時は以下を使用)
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://localhost/bookmanager'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'postgres://localhost/bookmanager'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
