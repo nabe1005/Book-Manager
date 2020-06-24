6458990300acc5fe1bf20b0e07329c91c081e320
Taiki Watanabe <taiki.w.1113@gmail.com>
2020-06-24 21:49:38 +0900

    一部ルーティングのコメントアウト

--- application/books.py
+++ application/books.py
@@ -9,13 +9,15 @@ books = Blueprint('books', __name__)
 @books.route('/books/index')
 @login_required
 def index():
-    series_n = Book.query.filter_by(user_id=current_user.id).order_by(Book.series_id.desc()).first().series_id
+    series_n = Book.query.filter_by(user_id=current_user.id).order_by(
+        Book.series_id.desc()).first().series_id
     series_n += 1
     res = dict()
 
     # シリーズの分だけ繰り返し
     for i in range(1, series_n):
-        books = Book.query.filter_by(series_id=i, user_id=current_user.id).order_by(Book.vol).all()
+        books = Book.query.filter_by(
+            series_id=i, user_id=current_user.id).order_by(Book.vol).all()
         display_vol = ''
 
         # 巻数の表示
@@ -25,11 +27,13 @@ def index():
                 if book.vol != 1:
                     if book.vol != books[-1].vol:
                         if prev_vol + 1 != book.vol:
-                            display_vol = display_vol[:-1] + ', ' + str(book.vol) + '~'
+                            display_vol = display_vol[:-1] + \
+                                ', ' + str(book.vol) + '~'
                         prev_vol = book.vol
                     else:
                         if prev_vol + 1 != book.vol:
-                            display_vol = display_vol[:-1] + ', ' + str(book.vol) + '巻'
+                            display_vol = display_vol[:-1] + \
+                                ', ' + str(book.vol) + '巻'
                         else:
                             display_vol += str(book.vol) + '巻'
                 else:
@@ -40,7 +44,8 @@ def index():
         for category in Book.query.filter_by(series_id=i, user_id=current_user.id).order_by(Book.vol).first().categories:
             categories.append(category.category_name)
 
-        book = {i: {'name': book.book_name, 'vol': display_vol, 'categories': categories}}
+        book = {i: {'name': book.book_name,
+                    'vol': display_vol, 'categories': categories}}
         res.update(book)
 
     return render_template('books_index.html', books=current_user.books, res=jsonify(res), title='本の一覧')
@@ -80,7 +85,8 @@ def register_post():
     # Bookインスタンスの作成
     new_books = []
     if Book.query.all():
-        series_id = Book.query.order_by(Book.series_id.desc()).first().series_id + 1
+        series_id = Book.query.order_by(
+            Book.series_id.desc()).first().series_id + 1
     else:
         series_id = 1
 
@@ -88,7 +94,8 @@ def register_post():
         if Book.query.filter_by(book_name=book_name, user_id=current_user.id).first():
             flash('既に追加されています')
             return redirect(url_for('books.register'))
-        new_book = Book(series_id=series_id, book_name=book_name, vol=1, user_id=current_user.id)
+        new_book = Book(series_id=series_id, book_name=book_name,
+                        vol=1, user_id=current_user.id)
         new_books.append(new_book)
     else:
         for i in range(int(vol)):
@@ -96,7 +103,8 @@ def register_post():
                 flash('既に追加されています')
                 return redirect(url_for('books.register'))
             if request.form.get('vol' + str(i + 1)):
-                new_book = Book(series_id=series_id, book_name=book_name, vol=i + 1, user_id=current_user.id)
+                new_book = Book(
+                    series_id=series_id, book_name=book_name, vol=i + 1, user_id=current_user.id)
                 new_books.append(new_book)
 
     # Categoriesの追加
@@ -119,7 +127,8 @@ def register_post():
 @books.route('/books/edit/<int:series_id>', methods=['GET'])
 @login_required
 def edit(series_id):
-    edit_books = Book.query.filter_by(series_id=series_id, user_id=current_user.id).order_by(Book.vol).all()
+    edit_books = Book.query.filter_by(
+        series_id=series_id, user_id=current_user.id).order_by(Book.vol).all()
 
     if not edit_books:
         flash('無効なURLです')
@@ -153,21 +162,25 @@ def edit_post(series_id):
     # 更新データの用意
     add_books = []
     if len(Book.query.filter_by(series_id=series_id, user_id=current_user.id).all()) > int(vol):
-        i_length = len(Book.query.filter_by(series_id=series_id, user_id=current_user.id).all()) + 1
+        i_length = len(Book.query.filter_by(
+            series_id=series_id, user_id=current_user.id).all()) + 1
     else:
         i_length = int(vol) + 1
 
     for i in range(1, i_length):
         if request.form.get('vol' + str(i)) and i <= int(vol):
             if Book.query.filter_by(series_id=series_id, vol=i, user_id=current_user.id).first():
-                add_book = Book.query.filter_by(series_id=series_id, vol=i, user_id=current_user.id).first()
+                add_book = Book.query.filter_by(
+                    series_id=series_id, vol=i, user_id=current_user.id).first()
                 add_book.book_name = book_name
                 add_books.append(add_book)
             else:
-                add_books.append(Book(book_name=book_name, series_id=series_id, vol=i, user_id=current_user.id))
+                add_books.append(
+                    Book(book_name=book_name, series_id=series_id, vol=i, user_id=current_user.id))
         else:
             if Book.query.filter_by(series_id=series_id, vol=i, user_id=current_user.id).first():
-                Book.query.filter_by(series_id=series_id, vol=i, user_id=current_user.id).delete()
+                Book.query.filter_by(series_id=series_id,
+                                     vol=i, user_id=current_user.id).delete()
 
     add_books[0].categories.clear()
     for category in current_user.categories:
@@ -189,7 +202,8 @@ def edit_post(series_id):
 @books.route('/books/delete/<int:series_id>')
 @login_required
 def delete(series_id):
-    delete_books = Book.query.filter_by(series_id=series_id, user_id=current_user.id).all()
+    delete_books = Book.query.filter_by(
+        series_id=series_id, user_id=current_user.id).all()
 
     if not delete_books:
         flash('無効なURLです')
@@ -238,54 +252,54 @@ def delete(series_id):
 #     return redirect(url_for('main.index'))
 
 
-# Categoryダミーデータ作成
-@books.route('/seed/c')
-def seed_c():
-    f = open('application/cate.txt')
-    c = f.readlines()
-    f.close()
+# # Categoryダミーデータ作成
+# @books.route('/seed/c')
+# def seed_c():
+#     f = open('application/cate.txt')
+#     c = f.readlines()
+#     f.close()
 
-    new_categories = []
+#     new_categories = []
 
-    for category in c:
-        new_categories.append(Category(category_name=category, user_id=1))
+#     for category in c:
+#         new_categories.append(Category(category_name=category, user_id=1))
 
-    try:
-        db.session.add_all(new_categories)
-        db.session.commit()
-    except Exception:
-        db.session.rollback()
-    finally:
-        db.session.close()
+#     try:
+#         db.session.add_all(new_categories)
+#         db.session.commit()
+#     except Exception:
+#         db.session.rollback()
+#     finally:
+#         db.session.close()
 
-    return redirect(url_for('main.index'))
-
-
-# Bookリセット
-@books.route('/delete/b')
-def delete_b():
-    try:
-        for book in Book.query.all():
-            db.session.delete(book)
-        db.session.commit()
-    except Exception:
-        db.session.rollback()
-    finally:
-        db.session.close()
-
-    return redirect(url_for('main.index'))
+#     return redirect(url_for('main.index'))
 
 
-# Categoryリセット
-@books.route('/delete/c')
-def delete_c():
-    try:
-        for category in Category.query.all():
-            db.session.delete(category)
-        db.session.commit()
-    except Exception:
-        db.session.rollback()
-    finally:
-        db.session.close()
+# # Bookリセット
+# @books.route('/delete/b')
+# def delete_b():
+#     try:
+#         for book in Book.query.all():
+#             db.session.delete(book)
+#         db.session.commit()
+#     except Exception:
+#         db.session.rollback()
+#     finally:
+#         db.session.close()
 
-    return redirect(url_for('main.index'))
+#     return redirect(url_for('main.index'))
+
+
+# # Categoryリセット
+# @books.route('/delete/c')
+# def delete_c():
+#     try:
+#         for category in Category.query.all():
+#             db.session.delete(category)
+#         db.session.commit()
+#     except Exception:
+#         db.session.rollback()
+#     finally:
+#         db.session.close()
+
+#     return redirect(url_for('main.index'))
